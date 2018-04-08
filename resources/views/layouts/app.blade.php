dfecfe038fc9f1ea0833b1a890b70d8319059c66
Raymon den Hartogh <raydenhartogh@gmail.com>
2018-04-08 12:32:59 +0200

    menu werkend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,13 +2,17 @@
 <html>
     <head>
         <title>{{config('app.name', 'Oefening Laravel')}}</title>
-        <link rel=stylesheet href="{{asset('css/style.css')}}">
+        <!--<link rel=stylesheet href="{{asset('css/style.css')}}">-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <style>
      
         </style>
     </head>
     <body>
-         @yield('content')
+        @include('inc.navbar')
+        <div class=container>
+            @yield('content')
+        </div>
     </body>
 </html>
 
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
         <h1>{{$title}}</h1>
-            <ul>
+            <ul class=list-group>
             @if(count($services) > 0)
                 @foreach($services as $service)
-                   <li>{{$service}}</li>
+                   <li class=list-group-item>{{$service}}</li>
                 @endforeach
             @endif
             </ul>
